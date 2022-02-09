97770a489f6d4237ce7b3371dfc5bfe4da9e90d9
doomydot <olof.hagstrom@gmail.com>
2022-02-10 00:03:30 +0100

    e10 progress

--- ch4/c04e10.cpp
+++ ch4/c04e10.cpp
@@ -8,15 +8,63 @@
 // maybe you should let the user enter some values. Try variations to make it
 // less easy for the user to guess which move the machine will make next.
 
+/*
+Comments:
+I'm not completely sure how the cin.ignore() works. I get that I need to flush the cin
+for the next iteration of the loop. But I fail to understand why I need both cin.clear + cin.ignore
+*/
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
+vector<char> comp_moves;
+int num_moves = 0;
 
+void game_setup(){
+    cout << "Rock, Paper, Scissors.\n";
+    cout << "--------------\n";
+    cout << "+ Setup +\n";
+    
+    while (num_moves < 1){
+        cout << "Enter how many moves you want the computer to make: ";
+        cin >> num_moves;
+        if (cin.fail()){
+            cout << "You must enter a valid number!\n";
+            cin.clear();
+            cin.ignore(256, '\n');
+        }   
+    }
+
+    cout << "Enter the moves of the computer (r, p, s). One move per message.\n";
+
+    for (int i = 0; i < num_moves; i++){
+        char temp = 0;
+        cout << "Move #" << i+1 << ": ";
+        if (cin >> temp && (temp == 'r' || temp == 'p' || temp == 's')){
+            comp_moves.push_back(temp);
+        }
+        else{
+            cout << "You must provide valid input (r, p, s).\n";
+            cin.clear();
+            cin.ignore(256, '\n');
+            --i;
+        }
+    }
+}
+
+void gameloop(){
+
+}
 
 int main()
-{
-    
+{   
+    string machine_feed = "";
+    string temp = "";
+
+
+    game_setup();
+    gameloop();
+
 }
+
