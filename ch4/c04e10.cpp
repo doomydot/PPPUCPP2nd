f810e55fc62c967509d8f5af983591079db51aeb
doomydot <olof.hagstrom@gmail.com>
2022-02-10 13:08:17 +0100

    rock paper scissor finished

--- ch4/c04e10.cpp
+++ ch4/c04e10.cpp
@@ -10,16 +10,34 @@
 
 /*
 Comments:
+    This ended up being somewhat more advanced than I anticipated,
+    but I'm pretty happy with the end result. If you follow along
+    you should read up on cin.clear() and cin.ignore() and how what
+    numeric_limits<streamsize>::max() does.
 
+    But basically I just divide the excersize into 3 parts, setup,
+    gameloop and round calculation. 
+    I also keep track of stats and display them to the user at the end.
 */
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <map>
 
 using namespace std;
 
 vector<char> comp_moves;
+map<char, string> moves = {
+    {'r', "rock"},
+    {'p', "paper"},
+    {'s', "scissor"}
+};
+
 int num_moves = 0;
+
+int wins = 0;
+int losses = 0;
+int draws = 0;
 
 void game_setup(){
     cout << "Rock, Paper, Scissors.\n";
@@ -39,7 +57,7 @@ void game_setup(){
     cout << "Enter the moves of the computer (r, p, s). One move per message.\n";
 
     for (int i = 0; i < num_moves; i++){
-        char temp = 0;
+        char temp = ' ';
         cout << "Move #" << i+1 << ": ";
         if (cin >> temp && (temp == 'r' || temp == 'p' || temp == 's')){
             comp_moves.push_back(temp);
@@ -53,9 +71,60 @@ void game_setup(){
     }
 }
 
-void gameloop(){
-
+char calculate_round(char user, char comp){
+    switch(user){
+        case 'r':
+            if (comp == 'r') return 'd';
+            else if (comp == 'p') return 'l';
+            else if (comp == 's') return 'w';
+            break;
+        case 'p':
+            if (comp == 'r') return 'w';
+            else if (comp == 'p') return 'd';
+            else if (comp == 's') return 'l';
+            break;
+        case 's':
+            if (comp == 'r') return 'l';
+            else if (comp == 'p') return 'w';
+            else if (comp == 's') return 'd';
+            break;
+    }
 }
+
+void gameloop(){
+    
+    for (int i = 0; i < size(comp_moves); i++){
+        char move = ' ';
+        cout << "Enter your move (r, p, s): ";
+        cin >> move;
+        if (!cin.fail() && (move == 'r' || move == 'p' || move == 's')){
+            
+            switch (calculate_round(move, comp_moves[i]))
+            {
+            case 'w': //win
+                cout << "You played " << moves[move] << " and the computer played " << moves[comp_moves[i]] << ". Resulting in a win!\n";
+                ++wins;
+                break;
+            case 'l': //loss
+                cout << "You played " << moves[move] << " and the computer played " << moves[comp_moves[i]] << ". Resulting in a loss!\n";
+                ++losses;
+                break;
+            case 'd': //draw
+                cout << "You played " << moves[move] << " and the computer played " << moves[comp_moves[i]] << ". Resulting in a draw!\n";
+                ++draws;
+                break;
+            }
+        }
+        else{
+            cout << "You must provide valid input (r, p, s).\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            --i;
+        }
+    }
+}
+
+
 
 int main()
 {   
@@ -65,6 +134,10 @@ int main()
 
     game_setup();
     gameloop();
+
+    cout << "The game is finished.\n";
+    cout << "Wins: " << wins << " | Losses: " << losses << " | Draws: " << draws << endl; 
+    
 
 }
 
