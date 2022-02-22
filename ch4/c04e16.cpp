6692aee706682ac3986482acdda0b89164d468aa
doomydot <olof.hagstrom@gmail.com>
2022-02-22 12:45:18 +0100

    start of c04e16

---
+++ ch4/c04e16.cpp
@@ -0,0 +1,24 @@
+//c04e16
+// In the drill, you wrote a program that, given a series of numbers, found the
+// max and min of that series. The number that appears the most times in
+// a sequence is called the mode. Create a program that finds the mode of a set
+// of positive integers.
+
+/*
+    Comments:
+    implement this solution
+    https://stackoverflow.com/questions/2488941/find-which-numbers-appears-most-in-a-vector
+
+*/
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int max = 0;
+
+    cout << "How many numbers do you want to enter: ";
+    cin >> max;
+}
