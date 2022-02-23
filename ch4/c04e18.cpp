19a321c7da71fbc45670aebd48b76aaea1dea681
doomydot <olof.hagstrom@gmail.com>
2022-02-23 20:50:57 +0100

    start of c04e18

---
+++ ch4/c04e18.cpp
@@ -0,0 +1,15 @@
+// c04e18
+//
+// Write a program to solve quadratic equations. A quadratic equation is of the
+// form:    ax^2 + bx + c = 0
+// If you don't know the quadratic formula for solving such an expression, do
+// some research. Remenber, researching how to solve a problem is often
+// necessary before a programmer can teach the computer how to solve it. Use
+// doubles for the user input for a, b and c. Since there are two solutions to
+// a quadratic equation, output both x1 and x2.
+
+#include <iostream>
+
+int main(){
+
+}
